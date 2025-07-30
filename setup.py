036ea777ea4e4fcf5afe50182d7b058c2eea444f
fernandoACF28 <fernando.costa@fisica.ufmt.br>
2025-07-30 14:50:47 -0300

    Organize_setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setup import setup, find_packages
+from setuptools import setup, find_packages
 
-setup(name='Valuate_Array',
-      version='0.1'
-      packpage=find_packages(),
-      author='Fernando Fernandes',
-      author_email='fernando.allysson@usp.br
-      description='This packpage have utils functions to validate products of satellite or var from reanalysis',
-      url='https://github.com/fernandoACF28/Valuate_Array',
-      classifiers=[
-      'Programming Language :: Python :: 3',
-      'License :: OSI Approved :: MIT License',
-    ])
+setup(
+    name='Valuate_Array',
+    version='0.1',
+    packages=find_packages(),
+    author='Fernando Fernandes',
+    author_email='fernando.allysson@usp.br',
+    description='This package has utility functions to validate products of satellite or variables from reanalysis',
+    url='https://github.com/fernandoACF28/Valuate_Array',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
