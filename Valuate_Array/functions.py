ff9530c9c05bb34d5b31955144ec6da8538a66c5
fernandoACF28 <fernando.costa@fisica.ufmt.br>
2025-07-30 16:36:45 -0300

    add sequential checking

--- Valuate_Array/functions.py
+++ Valuate_Array/functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import xarray as xr
 
 def select_pixels(ds,var:str,
                   station: tuple[float, float],
@@ -78,15 +79,49 @@ def Getting_dataframe_from_netCDF(path_netCDF,
 
 
 
-from glob import glob as gb 
-import xarray as xr
-
-"""
-# here some example to get dataframe from netCDF file
-window_sizes = [1, 5, 11, 25]
-
-files_nc = gb('/home/fernando/Documents/Dark Target HDF Files/MYD04_L2_netCDF_final/2002/*.nc')
-ds = xr.open_dataset(files_nc[7])
-
-Getting_dataframe_from_hdf(ds,'AOD_550nm','time',estacoes_america_sul,window_sizes,'teste')
-"""
+def Getting_dataframe_from_hdf_sequential(ds,
+                               var_ds: str,
+                               coordinate_time: str,
+                               dict_stations: dict,
+                               window_sizes: list[int],
+                               path_out: str):
+    data_list = []
+    size_time = len(ds[var_ds].time)
+    
+    for station in dict_stations:
+        coords_station, station_name = dict_stations[station], station
+        
+        for i_time in range(size_time):
+            dictionary_station = {
+                'station': station_name,
+                f'{coordinate_time}': ds[f'{coordinate_time}'].values[i_time]
+            }
+            
+            sequence_broken = False 
+            for win in sorted(window_sizes): 
+                
+                if sequence_broken:
+                    dictionary_station[f'mean_px_{win}x{win}'] = np.nan
+                    dictionary_station[f'std_px_{win}x{win}'] = np.nan
+                    continue
+                
+                ds_filled = select_pixels(ds=ds, var=[var_ds],
+                                          station=coords_station, window_size=win)
+                
+                arr = ds_filled[var_ds].isel(time=i_time).values
+                valid_pixels = int(np.sum(~np.isnan(arr)))
+                valid_win = valid_windows(win)
+                
+                if isinstance(valid_win, int) and valid_pixels >= valid_win:
+                    val_mean, val_std = get_mean_and_STD(ds_filled, var_ds, i_time)
+                    dictionary_station[f'mean_px_{win}x{win}'] = val_mean
+                    dictionary_station[f'std_px_{win}x{win}'] = val_std
+                else:
+                    dictionary_station[f'mean_px_{win}x{win}'] = np.nan
+                    dictionary_station[f'std_px_{win}x{win}'] = np.nan
+                    sequence_broken = True 
+            data_list.append(dictionary_station)
+    
+    df = pd.DataFrame(data_list)
+    os.makedirs('Parquet_datas', exist_ok=True)
+    df.to_parquet(f'Parquet_datas/{path_out}.parquet', index=False)
